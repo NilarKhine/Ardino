ef5bd38ab1cd6b854b00255d2050ee25b78ce2fb
NilarKhine <106440245+NilarKhine@users.noreply.github.com>
2022-05-28 23:23:17 +0630

    Add files via upload

---
+++ HomeMonitarSystemForElderPerson/Home_Monitoring_systemforelderlyperson_code/Home_Monitoring_systemforelderlyperson_code.ino
@@ -0,0 +1,102 @@
+
+#include <Stepper.h>
+#include <SoftwareSerial.h>
+
+#define STEPS_PER_MOTOR_REVOLUTION 32   
+
+//---( Steps per OUTPUT SHAFT of gear reduction )---
+#define STEPS_PER_OUTPUT_REVOLUTION 32 * 64  //2048 
+const int stepsPerRevolution = 570;  // change this to fit the number of steps per revolution
+// for your motor
+SoftwareSerial BlueTooth(2, 3); // (TXD, RXD) of HC-06
+// initialize the stepper library on pins 8 through 11:
+Stepper myStepper1(stepsPerRevolution, 8, 9, 10, 11);
+Stepper small_stepper(STEPS_PER_MOTOR_REVOLUTION, 4, 6,5,7);
+
+/*-----( Declare Variables )-----*/
+int  Steps2Take;
+char BT_input; // to store input character received via BT.
+
+int stepCount = 5.7;  // number of steps the motor has taken
+boolean stepperFlag= false;
+
+void setup() {
+  // put your setup code here, to run once:
+ BlueTooth.begin(9600); 
+ Serial.begin(9600);
+  myStepper1.setSpeed(10);
+  pinMode(A1,OUTPUT);
+
+   
+}
+//Start light system
+void loop() 
+{
+  if (BlueTooth.available())
+ 
+  {
+    BT_input=(BlueTooth.read());
+    if (BT_input=='a')
+    {
+      digitalWrite(A1, HIGH);
+      BlueTooth.println("Now LED is ON");
+    }
+    else if (BT_input=='b')
+    {
+      digitalWrite(A1, LOW);
+      BlueTooth.println("Now LED is OFF");
+    }
+   else if (BT_input=='?')
+    {
+      BlueTooth.println("Send 'a' to turn LED ON");
+      BlueTooth.println("Send 'b' to turn LED OFF");
+    }   
+    if (BT_input=='e'){
+ // step one revolution  in one direction:
+  Serial.println("clockwise");
+  myStepper1.step(stepsPerRevolution);
+ 
+    }
+  
+  else if (BT_input=='f'){
+  // step one revolution in the other direction:
+  Serial.println("counterclockwise");
+  myStepper1.step(-stepsPerRevolution);
+  
+}
+ else if (BT_input=='?')
+    {
+      BlueTooth.println("Send 'e' to the upstair");
+      BlueTooth.println("Send 'f' to the down stair");
+    }
+
+    if (BT_input=='c')
+    {
+  Steps2Take  =  STEPS_PER_OUTPUT_REVOLUTION ;  // Rotate CW 1 turn
+  small_stepper.setSpeed(1000);   
+  small_stepper.step(Steps2Take);
+  delay(1000);
+    }
+  
+   if (BT_input=='d')
+    {
+  Steps2Take  =  - STEPS_PER_OUTPUT_REVOLUTION;  // Rotate CCW 1 turn  
+  small_stepper.setSpeed(1000);  // 700 a good max speed??
+  small_stepper.step(Steps2Take);
+  delay(1000);
+    }
+    
+    if (BT_input=='?')
+    {
+      BlueTooth.println("Send 'c' to turn LED ON");
+      BlueTooth.println("Send 'd' to turn LED OFF");
+    }   
+   // You may add other if else condition here.
+    
+  }
+   }
+  
+
+
+ 
+  
---
+++ HomeMonitarSystemForElderPerson/bluetooth_motor/bluetooth_motor.ino
@@ -0,0 +1,53 @@
+// Bluetooth Tutorial By Robo India
+// Bluetooth module used - HC-06
+#include <Servo.h>
+#include <SoftwareSerial.h>
+SoftwareSerial BlueTooth(2, 3); // (TXD, RXD) of HC-06
+Servo myservo;
+
+int pos = 0;
+char BT_input; // to store input character received via BT.
+
+void setup()  
+{
+
+  BlueTooth.begin(9600); 
+  myservo.attach(13);  // attaches the servo on pin 9 to the servo object
+  pinMode(13,OUTPUT); 
+}
+
+void loop() 
+{
+  if (BlueTooth.available()>0)
+ 
+  {
+    BT_input=(BlueTooth.read());
+    if (BT_input=='c'){
+    for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(5);  // waits 15ms for the servo to reach the position
+    Serial.println("clockwise");
+  }
+    }
+  
+   
+    else if (BT_input=='d'){
+   for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(5);                       // waits 15ms for the servo to reach the position
+   Serial.println("counterclockwise");
+  }
+    }
+  }
+   else if (BT_input=='?')
+    {
+      BlueTooth.println("Send 'c' to turn LED ON");
+      BlueTooth.println("Send 'd' to turn LED OFF");
+    }   
+   // You may add other if else condition here. 
+  } 
+
+
+
+
